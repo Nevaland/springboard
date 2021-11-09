a6896073932fd140e072d485ed9693cb8c35f6b9
Nevaland <deanh0716@gmail.com>
2021-11-09 23:08:53 +0900

    Implement post service

--- src/main/java/nevaland/springboard/service/PostService.java
+++ src/main/java/nevaland/springboard/service/PostService.java
@@ -1,9 +1,12 @@
 package nevaland.springboard.service;
 
+import nevaland.springboard.domain.Post;
 import nevaland.springboard.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Transactional
 public class PostService {
@@ -13,5 +16,23 @@ public class PostService {
     @Autowired
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    /**
+     *  Write the post
+     */
+    public Long write(Post post) {
+        return null;
+    }
+
+    /**
+     *  Retrieve all post
+     */
+    public List<Post> findPosts() {
+        return postRepository.findAll();
+    }
+
+    public Optional<Post> findOne(Long postId) {
+        return postRepository.findById(postId);
     }
 }
