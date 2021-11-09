4457633ae78b41ff1a786feeaea32208d44f83e0
Nevaland <deanh0716@gmail.com>
2021-11-09 21:43:27 +0900

    Implement post write in Controller

--- src/main/java/nevaland/springboard/controller/BoardController.java
+++ src/main/java/nevaland/springboard/controller/BoardController.java
@@ -1,9 +1,11 @@
 package nevaland.springboard.controller;
 
+import nevaland.springboard.controller.form.PostForm;
 import nevaland.springboard.domain.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -21,11 +23,26 @@ public class BoardController {
 
     @GetMapping("/board/post")
     public String postDetail(@RequestParam Long id, Model model) {
+        // TODO: Get post from db instead of this
         Post post = new Post();
         post.setId(id);
         post.setTitle("title");
         post.setContent(("contentconent"));
         model.addAttribute("post", post);
         return "board/postDetail";
+    }
+
+    @GetMapping("/board/write")
+    public String writePostForm() {
+        return "board/writePost";
+    }
+
+    @PostMapping("/board/write")
+    public String writePost(PostForm postForm) {
+        Post post = new Post();
+        post.setTitle(postForm.getTitle());
+        post.setContent(postForm.getContent());
+        // TODO: Add post to db
+        return "redirect:/board";
     }
 }
