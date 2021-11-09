48b389e45e4f89347495114c9ac76d7a7c68e96a
Nevaland <deanh0716@gmail.com>
2021-11-09 22:39:27 +0900

    Implement memory post repository

---
+++ src/main/java/nevaland/springboard/repository/MemoryPostRepository.java
@@ -0,0 +1,35 @@
+package nevaland.springboard.repository;
+
+import nevaland.springboard.domain.Post;
+
+import java.util.*;
+
+public class MemoryPostRepository implements PostRepository {
+
+    private static Map<Long, Post> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Post save(Post post) {
+        post.setId(++sequence);
+        store.put(post.getId(), post);
+        return post;
+    }
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Post> findByTitle(String title) {
+        return store.values().stream()
+                .filter(post -> post.getTitle().equals(title))
+                .findAny();
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
---
+++ src/main/java/nevaland/springboard/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package nevaland.springboard.repository;
+
+import nevaland.springboard.domain.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostRepository {
+    Post save(Post post);
+    Optional<Post> findById(Long id);
+    Optional<Post> findByTitle(String title);
+    List<Post> findAll();
+}
